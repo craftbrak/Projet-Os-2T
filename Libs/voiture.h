61af95542d51bf81ef50de0cbb6898011454fa98
craftbrak <62942458+craftbrak@users.noreply.github.com>
2020-09-24 10:47:39 +0200

    creation voiture et fichier de base
    creation des structure de base

---
+++ Libs/voiture.h
@@ -0,0 +1,11 @@
+struct Voiture {
+    int numVoiture;
+    double S1,S2,S3;
+    double GAP;
+    double TotalTime;
+    int pit,out;
+    int pitConteur;
+    int KmParcouruPneu;
+    double ussurePneu;
+    int totalKmParcouru;
+};
