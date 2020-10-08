1a8f2b1b947c068ba2ca1eb89b97ea664b068fb9
craftbrak <62942458+craftbrak@users.noreply.github.com>
2020-10-08 13:13:49 +0200

    ajout du fork
    pas sur que ca fonctionne

---
+++ cmake-build-debug/CMakeFiles/fcc.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/Libs/Settings.c" "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/cmake-build-debug/CMakeFiles/fcc.dir/Libs/Settings.c.obj"
+  "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/Libs/SettingsParser.c" "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/cmake-build-debug/CMakeFiles/fcc.dir/Libs/SettingsParser.c.obj"
+  "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/Libs/course.c" "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/cmake-build-debug/CMakeFiles/fcc.dir/Libs/course.c.obj"
+  "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/Libs/randomLib.c" "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/cmake-build-debug/CMakeFiles/fcc.dir/Libs/randomLib.c.obj"
+  "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/main.c" "D:/OneDrive - EPHEC asbl/GitHub/Projet-Os-2T/cmake-build-debug/CMakeFiles/fcc.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include "config.h"
 #include "Libs/randomLib.h"
 #include "Libs/voiture.h"
@@ -16,18 +18,24 @@ void main () {
     char* noms[] = NOMS_VOITURES;
     size_t qteVoitures = sizeof noms / sizeof noms[0];
     Voiture voitures[qteVoitures];
-
+    pid_t pid[qteVoiture] ;
     initVoitures(&voitures, &noms, qteVoitures);
     setSeed(generateSeed());
 
     for (j=0;j<qteVoitures;j++) {
-        essai(&voitures[j], 3000, vitesseMoyenne, &longueurSections);
+        pid[j]=fork()
+        if (pid[j] != 0){
+            for (i=1;i<=QTE_SECTIONS;i++) {
+                printf("S%i : %lf s \n", i, voitures[j].sections[i-1]);
+            }
+        } else{
+            essai(&voitures[j], 3000, vitesseMoyenne, &longueurSections);
 
-        printf("Voiture %s: \n", voitures[j].nomVoiture);
-        for (i=1;i<=QTE_SECTIONS;i++) {
-             printf("S%i : %lf s \n", i, voitures[j].sections[i-1]);
+            printf("Voiture %s: \n", voitures[j].nomVoiture);
         }
+
     }
+
 }
 
 void initVoitures(Voiture (*voitures)[], char* (*noms)[], size_t size) {
