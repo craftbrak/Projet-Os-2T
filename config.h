1255b3823abba3e47ad4c2ceb994556e91f4defd
Mathieu Walravens <m.walravens@students.ephec.be>
2020-10-22 13:47:35 +0200

    Delete config.h

--- config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define NOMS_VOITURES {"44","77","16","5","33","23","55","4","3","31","10","26","11","18","7","99","20","8","63","6"}
-#define QTE_SECTIONS 3
-#define LONGUEUR_SECTIONS {3, 5, 4}
-#define VITESSE_MOYENNE 250
-#define QTE_TOURS_FINALE 27
